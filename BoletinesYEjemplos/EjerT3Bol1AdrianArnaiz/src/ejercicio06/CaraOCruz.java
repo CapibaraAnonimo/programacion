bced01218f1c52e7e71cddccdcaf812e0e9d3793
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-11-17 14:07:25 +0100

    Terminado el ejercicio 06

--- BoletinesYEjemplos/EjerT3Bol1AdrianArnaiz/src/ejercicio06/CaraOCruz.java
+++ BoletinesYEjemplos/EjerT3Bol1AdrianArnaiz/src/ejercicio06/CaraOCruz.java
@@ -1,7 +1,11 @@
 package ejercicio06;
 
+import java.util.Random;
+
 public class CaraOCruz 
 {
+	private Random ran = new Random(System.nanoTime());
+	private int ranMin = 1, ranMax = 2;
 	
 	public CaraOCruz()
 	{
@@ -9,7 +13,35 @@ public class CaraOCruz
 	
 	public int lanzar()
 	{
-		return 1;
+		return ran.nextInt(ranMax-ranMin+1)+ranMin;
+		
+	}
+	
+	public void comprobar(int eleccion)
+	{
+		if(eleccion == lanzar())
+		{
+			mostrar(true);
+		}
+		else {
+			mostrar(false);
+		}
+	}
+	
+	public void mostrar(boolean ganador)
+	{
+		if(ganador)
+		{
+			System.out.println("-----------\n"
+							 + "|Ha Ganado|\n"
+							 + "-----------");
+		}
+		else
+		{
+			System.out.println(" ____________\n"
+							 + "/Siga Jugando\\\n"
+							 + "--------------");
+		}
 	}
 
 }
--- BoletinesYEjemplos/EjerT3Bol1AdrianArnaiz/src/ejercicio06/Principal.java
+++ BoletinesYEjemplos/EjerT3Bol1AdrianArnaiz/src/ejercicio06/Principal.java
@@ -1,10 +1,33 @@
 package ejercicio06;
 
+import lectura.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) 
 	{
+		int eleccion = 0;
+		CaraOCruz game = new CaraOCruz();
 		
+		System.out.println("Este programa simula el lanzamiento de una moneda");
+		
+		do
+		{
+			System.out.println("\nIntroduzca 1 para cara o 2 para cruz");
+			eleccion = Leer.datoInt();
+			while(eleccion != 1 || eleccion != 2)
+			{
+				System.out.println("Número incorrecto, introduzca otro");
+				eleccion = Leer.datoInt();
+			}
+			
+			if(eleccion != 0)
+			{
+				game.comprobar(eleccion);
+			}
+		}while(eleccion != 0);
+		
+		System.out.println("Gracias por jugar");
 
 	}
 
