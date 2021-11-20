fe0763ef0503ad61312dd0d929c7657997938ee9
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2021-11-20 20:30:46 +0100

    Se ha hecho el ejercicio 01 de Entornos de Desarrollo

---
+++ BoletinesYEjemplos/Debug/src/e01/PruebaDebugger.java
@@ -0,0 +1,13 @@
+package e01;
+
+public class PruebaDebugger {
+
+	public static void main(String[] args) 
+	{
+		ClaseParaDepurar clase = new ClaseParaDepurar();
+    	double valor = clase.echaCuentas();
+    	System.out.println("Sale "+valor);
+
+	}
+
+}
