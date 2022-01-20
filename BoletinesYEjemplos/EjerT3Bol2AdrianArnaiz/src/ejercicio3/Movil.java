2fe0f2ed2d45df1f0593289d58a37298c4f8577b
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2022-01-20 08:48:18 +0100

    Hechas las clases del ejercicio 3

--- BoletinesYEjemplos/EjerT3Bol2AdrianArnaiz/src/ejercicio3/Movil.java
+++ BoletinesYEjemplos/EjerT3Bol2AdrianArnaiz/src/ejercicio3/Movil.java
@@ -5,5 +5,50 @@ public class Movil {
 	private String modelo;
 	private boolean vendido;
 	private double precioUnitario;
+	
+	public Movil(String marca, String modelo, double precioUnitario) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.vendido = false;
+		this.precioUnitario = precioUnitario;
+	}
 
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public boolean isVendido() {
+		return vendido;
+	}
+
+	public void setVendido(boolean vendido) {
+		this.vendido = vendido;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+	
+	@Override
+	public String toString() {
+		return "Marca: " + marca + "\nModelo: " + modelo + "\nPrecio: " + precioUnitario;
+	}
+	
 }
--- BoletinesYEjemplos/EjerT3Bol2AdrianArnaiz/src/ejercicio3/Vendedor.java
+++ BoletinesYEjemplos/EjerT3Bol2AdrianArnaiz/src/ejercicio3/Vendedor.java
@@ -4,5 +4,29 @@ public class Vendedor {
 	private Movil lista[];
 	private int ventas;
 	
+	public Vendedor(Movil lista[]) {
+		this.lista = lista;
+	}
+	
+	public void movilesRestantes() {
+		for(int i = 0; i < lista.length; i++)
+		{
+			if(!lista[i].isVendido())
+				System.out.println((i+1) + ")\n" + lista[i]);
+		}
+	}
+
+	public double precioDescuento(int movil, int descuento) {
+		return lista[movil-1].getPrecioUnitario() * (1-(descuento/100));
+	}
+	
+	public void vender(int movil) {
+		if(!lista[movil].isVendido()) {
+			lista[movil].setVendido(true);
+			ventas++;
+		}
+		else
+			System.out.println("No está disponible ese movil");
+	}
 
 }
