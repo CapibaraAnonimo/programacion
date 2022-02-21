26aa1c6e9a0c9074bdd2c151bfdc94e6c03807e6
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2022-02-21 11:33:30 +0100

    Hecho el ejemplo de listas

---
+++ BoletinesYEjemplos/EjemplosTema5/src/ejemploList/Principal.java
@@ -0,0 +1,17 @@
+package ejemploList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+	public static void main(String[] args) {
+		List <String>lista = new ArrayList<String> ();
+		List <Integer>lista2 = new ArrayList<Integer> ();
+		lista.add("Adrián");
+		lista2.add(91);
+		
+		System.out.println(lista);
+		System.out.println(lista2);
+	}
+
+}
