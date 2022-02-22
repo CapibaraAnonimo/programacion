b4f9dc3e134a6e90683c13a35b7095a4bebe7aa5
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2022-02-22 14:26:23 +0100

    hecho ejemplo Sets

---
+++ BoletinesYEjemplos/EjemplosTema5/src/EjemploSet/Principal.java
@@ -0,0 +1,14 @@
+package EjemploSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Set<String> lista = new HashSet<String>();
+
+		lista.add("adrián");
+	}
+
+}
