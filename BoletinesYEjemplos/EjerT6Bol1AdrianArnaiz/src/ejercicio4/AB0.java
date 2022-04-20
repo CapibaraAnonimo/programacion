d0686f06310ed10775e3e9955f29f10c37b952e6
CapibaraAnonimo <adrianarnaizcano@gmail.com>
2022-04-20 13:01:35 +0200

    A medias del ejercicio 4 de excepciones

---
+++ BoletinesYEjemplos/EjerT6Bol1AdrianArnaiz/src/ejercicio4/AB0.java
@@ -0,0 +1,14 @@
+package ejercicio4;
+
+public class AB0 extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public AB0() {
+		System.err.println("La ecuación es degenerada");
+	}
+
+}
---
+++ BoletinesYEjemplos/EjerT6Bol1AdrianArnaiz/src/ejercicio4/CEcuacion2Grado.java
@@ -0,0 +1,23 @@
+package ejercicio4;
+
+public class CEcuacion2Grado {
+	private double a;
+	private double b;
+	private double c;
+	
+	public CEcuacion2Grado(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double[] calcularRaices() {
+		double raices[] = new double[2];
+		return raices;
+	}
+	
+	public double discriminante() {
+		return 12;
+	}
+
+}
